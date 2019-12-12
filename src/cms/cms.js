0c62638b536cd42f968e5a508ecbb775292fbbbf
Michael Bonner <michaelwbonner@gmail.com>
2019-12-12 15:27:07 -0700

    add story type

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,8 +4,10 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import StoryPreview from './preview-templates/StoryPreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('story', StoryPreview)
